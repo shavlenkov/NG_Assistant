50c954db9ef4fce8c21f5e7003dd95c71a07d513
shavlenkov <pashashavlenkov@gmail.com>
2025-01-19 19:12:36 +0200

    updated the prompt for context

--- src/aiworker.cpp
+++ src/aiworker.cpp
@@ -20,8 +20,17 @@ void AIWorker::sendOllamaRequest(QString prompt)
 
     if(m_context.empty()) {
         json["prompt"] = "You are the Linux assistant. "
-            "If the text is about creating, deleting or renaming a folder or file, just reply with a Linux command (without extra text), "
-            "and if it is about something else, just reply to the message.";
+                         "If the text is about creating, deleting, renaming, or moving a folder or a file, just reply with a Linux command (without extra text, "
+                         "all paths must begin with \"~/\", example: "
+                         "creating a folder: mkdir ~/folder, mkdir ~/Desktop/folder "
+                         "crearing a file: touch ~/file.txt, touch ~/Desktop/file.txt "
+                         "deleting a folder: rm -Rf ~/folder, rm -Rf ~/Desktop/folder "
+                         "deleting a file: rm -f ~/file.txt, rm -f ~/Desktop/file.txt "
+                         "renaming a folder: mv ~/old_folder ~/new_folder, mv ~/Desktop/old_folder ~/Desktop/new_folder "
+                         "renaming a file: mv ~/old_file.txt ~/new_file.txt, mv ~/Desktop/old_file.txt ~/Desktop/new_file.txt "
+                         "moving a folder: mv ~/folder ~/Desktop/, mv ~/Desktop/folder ~/ "
+                         "moving a file: mv ~/file.txt ~/Desktop/, mv ~/Desktop/file.txt ~/), "
+                         "and if it is about something else, just reply to the message.";
     } else {
         json["prompt"] = prompt;
         json["context"] = m_context;
